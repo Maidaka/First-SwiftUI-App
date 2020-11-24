788faa295c226b33314e8ed0a7487d5bf13ef30c
Maida <maida.karahmetovic@gmail.com>
2020-11-24 20:58:01 +0100

    Initial Commit

---
+++ I-am-Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I-am-Rich
+//
+//  Created by Maida on 11/24/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
